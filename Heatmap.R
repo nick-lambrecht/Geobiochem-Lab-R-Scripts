fdd30847aa0932cb8b282572bed8a549e43e4b8b
nick-lambrecht <58522380+nick-lambrecht@users.noreply.github.com>
2019-12-04 10:02:37 -0600

    Update Heatmap.R

--- Heatmap.R
+++ Heatmap.R
@@ -1,5 +1,4 @@
 #I followed this tutorial: https://sebastianraschka.com/Articles/heatmaps_in_r.html
-#The goal of this code was to create a heatmap to show the distribution of various Fe gene (and Fe gene families) across many metagenomic bins.
 library(gplots)
 library(RColorBrewer)
 #Set wd
