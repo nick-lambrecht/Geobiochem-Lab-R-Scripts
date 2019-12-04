fc89e2a92181d0ba1711509ef507e7e9812f6ea5
nick-lambrecht <58522380+nick-lambrecht@users.noreply.github.com>
2019-12-04 09:47:07 -0600

    Add files via upload

---
+++ Heatmap.R
@@ -0,0 +1,38 @@
+#I followed this tutorial: https://sebastianraschka.com/Articles/heatmaps_in_r.html
+library(gplots)
+library(RColorBrewer)
+#Set wd
+#Read data in; my file was a .csv
+FeGenie <- read.csv("../fegenie_output/FeGenie-heatmap-data.csv")
+#Store rownames as an object
+rnames <- FeGenie[,1]
+#convert data frame into a matrix
+FeG_data <- data.matrix(FeGenie[,2:ncol(FeGenie)])
+#add row names back in
+rownames(FeG_data) <- rnames
+
+#making custom color palette
+my_palette <- colorRampPalette(c("lightgray", "yellow", "red"))(n = 299)
+# (optional) defines the color breaks manually for a "skewed" color transition
+col_breaks = c(seq(0,0.09,length=100),  # for lightgray
+               seq(0.1,0.5,length=100),           # for yellow
+               seq(0.51,1.20,length=100))             # for red
+#Saving heatmap as a PDF file
+pdf("../fegenie_output/heatmap.pdf",width = 10, height = 10, pointsize = 10)
+
+heatmap.2(FeG_data,
+          cellnote = FeG_data, #cellnote adds values in the boxes
+          main = "Fe Gene Families",
+          notecol = "black",
+          density.info="none",
+          trace="none",
+          margins =c(12,9),
+          col=my_palette,
+          breaks=col_breaks,
+          dendrogram = "none",
+          srtCol = 45,
+          Colv="NA",
+          Rowv = "NULL")
+dev.off()
+          
+    
